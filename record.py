468ca5f75eea23af8ccf9111bfb27291e9a53868
Matthew Zackey <149171771+MattZackey@users.noreply.github.com>
2024-02-27 07:55:48 +0200

    Add files via upload

--- record.py
+++ record.py
@@ -4,15 +4,20 @@ import pathlib
 import os
 import torch
 
-def record_image(agent_env, agent, num_iter, example_path):
+def record_image(agent_env, agent, num_iter, example_path, seed = None):
 
     if not os.path.exists(example_path):
         pathlib.Path(example_path).mkdir(parents=True, exist_ok=True)
 
-    env = gym.make(agent_env, render_mode="rgb_array")
+    env = gym.make(agent_env, render_mode = "rgb_array")
     agent.actor.eval()
-    state, info = env.reset()
-    state = torch.tensor(state)
+    
+    if seed:
+        state, info = env.reset(seed = seed)
+    else:
+        state, info = env.reset()
+    
+    state = torch.tensor(state, dtype = torch.float32)
     done = False
     frames = []
     while not done:
@@ -20,7 +25,7 @@ def record_image(agent_env, agent, num_iter, example_path):
         action = agent.actor(state.view(1, -1))[0]
         next_state, reward, terminated, truncated, _ = env.step(action.tolist())
         done = terminated or truncated
-        state = torch.tensor(next_state)
+        state = torch.tensor(next_state, dtype = torch.float32)
     env.close()
     
     imageio.mimsave(os.path.join(example_path, f'run{num_iter}.gif'), frames, fps=30)
